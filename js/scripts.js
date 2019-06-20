37789743e2f937099063ae2d1bca0720dcc5ef7d
Jessica Munoz and Na Hyung Choi <jessica+na@>
2019-06-20 13:39:12 -0700

    try refactoring determineWin logic

--- js/scripts.js
+++ js/scripts.js
@@ -37,26 +37,44 @@ var markSquare = function(clickedSqId){    // Marks clicked square with current 
   });
 }
 
+var winCombos = [
+  [0,4,8],
+  [2,4,6],
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+]
+
 var determineWin = function() {
-  if (squares[0].symbol === squares[4].symbol && squares[4].symbol === squares[8].symbol) {
-    winner = true;
-  } else if (squares[2].symbol === squares[4].symbol && squares[4].symbol === squares[6].symbol) {
-    winner = true;
-  } else if (squares[0].symbol === squares[1].symbol && squares[1].symbol === squares[2].symbol) {
-    winner = true;
-  } else if (squares[3].symbol === squares[4].symbol && squares[4].symbol === squares[5].symbol) {
-    winner = true;
-  } else if (squares[6].symbol === squares[7].symbol && squares[7].symbol === squares[8].symbol) {
-    winner = true;
-  } else if (squares[0].symbol === squares[3].symbol && squares[3].symbol === squares[6].symbol) {
-    winner = true;
-  } else if (squares[1].symbol === squares[4].symbol && squares[4].symbol === squares[7].symbol) {
-    winner = true;
-  } else if (squares[2].symbol === squares[5].symbol && squares[5].symbol === squares[8].symbol) {
-    winner = true;
-  } else {
-    winner = false;
+  for (var i = 0; i <= 7; i++) {
+    if (squares[winCombos[i][0]].symbol === squares[winCombos[i][1]].symbol && squares[winCombos[i][1]].symbol === squares[winCombos[i][2]].symbol) {
+      winner = true;
+    } else {
+      return;
+    }
   }
+  // if (squares[0].symbol === squares[4].symbol && squares[4].symbol === squares[8].symbol) {
+  //   winner = true;
+  // } else if (squares[2].symbol === squares[4].symbol && squares[4].symbol === squares[6].symbol) {
+  //   winner = true;
+  // } else if (squares[0].symbol === squares[1].symbol && squares[1].symbol === squares[2].symbol) {
+  //   winner = true;
+  // } else if (squares[3].symbol === squares[4].symbol && squares[4].symbol === squares[5].symbol) {
+  //   winner = true;
+  // } else if (squares[6].symbol === squares[7].symbol && squares[7].symbol === squares[8].symbol) {
+  //   winner = true;
+  // } else if (squares[0].symbol === squares[3].symbol && squares[3].symbol === squares[6].symbol) {
+  //   winner = true;
+  // } else if (squares[1].symbol === squares[4].symbol && squares[4].symbol === squares[7].symbol) {
+  //   winner = true;
+  // } else if (squares[2].symbol === squares[5].symbol && squares[5].symbol === squares[8].symbol) {
+  //   winner = true;
+  // } else {
+  //   winner = false;
+  // }
 }
 
 
@@ -64,10 +82,11 @@ var determineWin = function() {
 
 $(function(){
   $(".grid-item").click(function(){
+    debugger;
     var clickedSqId = $(this).prop("id");
     markSquare(clickedSqId);
+    determineWin();
   })
 
   //somewhere
-  determineWin();
 });
